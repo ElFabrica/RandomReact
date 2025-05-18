903cd8b73c182826c30815ec1adbc1797a6a1ada
Arthur <Arthur.fabricyo@gmail.com>
2025-05-18 15:17:55 -0300

    randomReactNative

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import React, {useState} from "react"
-import {View, Text, Image, Button, StyleSheet, TextInput} from "react-native"
+import {View, Text, TextInput, StyleSheet} from "react-native"
 
 function App(){
-  
+  const [nome, setNome] = useState('') 
+
+  function pegaNome(texto) {
+    if (!texto) {
+    setNome("")  
+    }
+    setNome(`Bem vindo ${texto}`)
+    
+  }
   
   return(
     
-    <View style= {{ flex: 1, flexDirection: "row", alignItems: "center",  justifyContent:"center"}}>
-
-      <View style= {{height: 50,width: 50, backgroundColor: "black"}}></View>
-      <View style= {{height: 50,width: 50,backgroundColor: "green"}}> </View>
-      <View style= {{height: 50,width: 50, backgroundColor: "red"}}></View>
+    <View sytle ={styles.conteiner}>
       
-  
+      <TextInput style = {styles.input} onChangeText={(text) => pegaNome(text)}> </TextInput>
 
-        
+      <Text style = {styles.text}>{nome}</Text>
 
-      
     </View>
-  )
-}
+
+    
+  )}
+const styles = StyleSheet.create({
+  conteiner:{
+  flex: 1,
+  paddingTop: 40,
+  marginTop: 40
+
+  },
+  input:{
+    height: 55,
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    fontSize: 20
+  },
+  text:{
+    textAlign: "center",
+    fontSize: 25
+  }
+})
+
 export default App
