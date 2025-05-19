fc38bf6a6051160db98ccf8ea910a61498ce8d93
emilly silveira <emillysilveiraeua@gmail.com>
2025-05-19 14:08:12 -0300

    app Contagem no restaurante

--- App.js
+++ App.js
@@ -1,91 +1,47 @@
-import React, { cloneElement, useState } from "react"
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
-
-let timer = null
-let ss = 0
-let mm = 0
-let hh = 0
+import React, { useState } from "react"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 
 function App() {
-  const [numero, setNumero] = useState(0)
-  const [botao, setBotao] = useState("INICIAR")
-  const [ultimo, setUltimo] = useState(null)
+  const [pessoaTotal, statePessoasTotal] = useState(10)
+  const [pessoas, statepessoas] = useState(0)
 
-function start(){
-  if(timer !== null){
-      //Aqui pausa o timer 
-      clearInterval(timer)
-      timer = null
-      setBotao("INICIAR")
-
-  }else{
-    //Reativar o timer
-    timer = setInterval(()=>{
-      ss++
-
-      if(ss == 60){
-        ss = 0
-        mm++
-      }
-      if(mm == 60){
-        mm = 0
-        hh++
-      }
-
-      let format = `${hh < 10 ? "0" + hh: hh}:${mm < 10 ? "0" + mm: mm}:${ss < 10 ? "0" + ss : ss}`
-
-      setNumero(format)
-    }, 1000)
-
-    setBotao("PAUSAR")
+  function adicionar() {
+    if (pessoas == pessoaTotal) {
+      return
+    }
+    statepessoas(pessoas + 1)
   }
-}
-function clean() {
-  if(timer !==null){
-    //Parar o timer
-    clearInterval(timer)
-    timer = null 
+
+  function diminuir() {
+    if (pessoas == 0) {
+      return
+    }
+    statepessoas(pessoas - 1)
+
   }
-  setUltimo(numero)
-  setNumero(0)
-  ss = 0
-  mm = 0
-  hh = 0
-
-  setBotao("INICIAR")
-
-}
   return (
 
     <View style={styles.conteiner}>
+      <Text style={styles.title}>Pessoas no restaurante</Text>
 
-      <Image source={require("./src/crono.png")} />
-
-      <Text style={styles.time}>{numero}</Text>
-
-      <View style={styles.btnArea}>
-
-        <TouchableOpacity style ={styles.btn} onPress={start}>
-          <Text style ={styles.btnText}>{botao}</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style ={styles.btn} onPress={clean}>
-          <Text style ={styles.btnText} >RESETAR</Text>
-        </TouchableOpacity>
-
+      <View style={styles.qtdPessoas}>
+        <Text style={{ color: "#FFFFFF", fontSize: 30, fontWeight: "bold" }}>{pessoas}</Text>
       </View>
-      <View style={styles.areaUltima}>
-        <Text style ={styles.textoCorrida}>
-          {ultimo ? `Ultimo tempo ${ultimo}`: "Nenhum tempo registrado..."}
-        </Text>
 
-      </View>
-      <View style ={{alignItems:"flex-end", justifyContent: "flex-end", width: "100%", alignContent: "flex-end", margin:"12", padding: 8  }}>
-      <Text style={{color: "#FFFFFF", fontSize:15}}>@El Fábrica Company</Text>
+      <Text style= {pessoas == pessoaTotal ? styles.textSucess : styles.textFail}>O restaurante está no seu limite de pessoas</Text>
+
+      <View style={styles.footer}>
+        <View>
+          <TouchableOpacity style={styles.btn} onPress={adicionar}>
+            <Text style={styles.textBtn}>Adicionar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.btn} onPress={diminuir}>
+          <Text style={styles.textBtn}>Remover</Text>
+        </TouchableOpacity>
       </View>
     </View>
-
-
   )
 }
 const styles = StyleSheet.create({
@@ -94,40 +50,51 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#00AEEF"
+    gap: 10
   },
-  time: {
-    marginTop: -160,
-    fontSize: 45,
-    fontWeight: "bold",
-    color: "#FFFFFF"
-  },
-  btnArea:{
+
+  footer: {
     flexDirection: "row",
-    marginTop: 130,
-    height: 40
+    gap: 20,
   },
-  btn:{
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "#FFFFFF",
-  height: 40,
-  margin: 17,
-  borderRadius: 9
-  },
-  btnText:{
+  title: {
     fontSize: 20,
-    fontWeight: "bold",
-    color: "#00AEEF"
+    fontWeight: "bold"
   },
-  areaUltima:{
-    marginTop: 40
+  qtdPessoas: {
+    color: "#FFFFFF",
+    backgroundColor: "#141414",
+    width: 60,
+    height: 80,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+
   },
-  textoCorrida:{
-      fontSize: 22,
-      color: "#FFFFFF",
-      fontStyle:"italic"
+  textFail:{
+    display: "none"
+  },
+  textSucess:{
+    backgroundColor: "#FCB135",
+    borderRadius: 10,
+    padding: 8
+  },
+
+  btn: {
+    backgroundColor: "#007AFE",
+    padding: 16,
+    borderRadius: 10
+  },
+  btnOpacity: {
+    backgroundColor: "#007AFE",
+    padding: 16,
+    borderRadius: 10,
+    opacity: 40
+
+  },
+  textBtn: {
+    color: "#FFFFFF",
+    fontSize: 20
   }
 })
 
